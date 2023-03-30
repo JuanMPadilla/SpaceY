57d1c85d941d7a1bfd57ad4fb2bb5a53de44c1eb
JuanMPadilla <46550859+JuanMPadilla@users.noreply.github.com>
2023-03-30 17:46:54 -0600

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,7 @@
+Python 3.10.10 (v3.10.10:aad5f6a891, Feb  7 2023, 08:47:40) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import pandas as pd
+>>> import numpy as np
+>>> 
+>>> print('Hello Capstone Project Course!')
+Hello Capstone Project Course!
